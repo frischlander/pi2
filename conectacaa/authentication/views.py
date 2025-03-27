e77fcc0f8179f64b7a6359040ff437de04633dcf
Marcio Júnior <73200722+devmarciojr@users.noreply.github.com>
2025-03-27 19:29:36 -0300

    Implementado resgistro sob permissão do admin

--- conectacaa/authentication/views.py
+++ conectacaa/authentication/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 class UsernameValidationView(View):
     def post(self, request):
@@ -14,6 +16,30 @@ class UsernameValidationView(View):
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'Usuário já existe'}, status=409)
         return JsonResponse({'Usuário válido': True})
+
+class LoginView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('caaordserv')
+        return render(request, 'authentication/register.html')
+    
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('caaordserv')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos')
+            return render(request, 'authentication/register.html')
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
 
 class RegistrationView(View):
     def get(self, request):
