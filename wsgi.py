6676686fbcccf1d390fdd604c3853b877d1e4248
André <frischlander@gmail.com>
2025-09-29 20:48:33 -0300

    alteração no arquivo settings e wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,14 @@
 import os
 import sys
 
-<<<<<<< HEAD
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-=======
-<<<<<<< HEAD
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-=======
-<<<<<<< HEAD
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+# Adiciona a raiz do projeto ao PYTHONPATH
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-=======
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
->>>>>>> 7e403d9a0edb7fc3fc0b12150895b5d2b76b65ce
->>>>>>> 03aaf15e87c501fbcfade3d3d0dc3cdf093cb61c
->>>>>>> 50c95bb53b28d10e607a7abcc0f289f657cb1e2d
+# Define as configurações do Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conectaa.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conectacaa.settings")
-
 application = get_wsgi_application()
+
 
