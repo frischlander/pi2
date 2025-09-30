e6290d49cd0ccf53f4af1c60c8e577fd5a58ff82
André <frischlander@gmail.com>
2025-09-29 21:07:53 -0300

    Corrije código

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Define as configurações do Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conectaa.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conectacaa.settings")
 
 from django.core.wsgi import get_wsgi_application
 
