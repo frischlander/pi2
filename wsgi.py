19666d3ff7299365120935c6bbb35f282ca9ba5d
André <frischlander@gmail.com>
2025-09-30 03:31:27 -0300

    Add .env file and update Django settings module references

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 
 # Define as configurações do Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'settings')
 
 from django.core.wsgi import get_wsgi_application
 
