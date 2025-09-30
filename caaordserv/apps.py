7174ba3912e0cef31517147d0848073101673ebc
André <frischlander@gmail.com>
2025-09-30 03:45:19 -0300

    atualiz

--- caaordserv/apps.py
+++ caaordserv/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CaaordservConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'conectacaa.caaordserv'
+    name = 'caaordserv'
