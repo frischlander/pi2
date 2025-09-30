1b35fed7a0d66294e52ef8f8ba5f584ccd7134ae
André <frischlander@gmail.com>
2025-09-30 11:13:52 -0300

    Fix database configuration to include default SQLite database path in settings.py

--- settings.py
+++ settings.py
@@ -85,7 +85,11 @@ WSGI_APPLICATION = 'wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
