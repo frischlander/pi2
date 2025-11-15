63dba2bbd96da5030c2b9b99522f1d58677eecb0
frischlander <frischlander@gmail.com>
2025-11-15 16:38:52 -0300

    CORRECAO 9

--- urls.py
+++ urls.py
@@ -2,13 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import views
 
-# Importações adicionais para gerenciamento programático
-from django.http import HttpResponse
-from django.core.management import call_command
-from django.contrib.auth import get_user_model
-import os # Importe o módulo os
+# Importação correta da view
+from caaordserv.views import sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +12,5 @@ urlpatterns = [
     path('authentication/', include('authentication.urls')),
     path('relatorios/', include('relatorios.urls')),
     path('', include('caaordserv.urls')),
-    path('sobre/', views.sobre, name='sobre'),
-
+    path('sobre/', sobre, name='sobre'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
