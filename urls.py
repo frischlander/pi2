6f96f7d4abda95c19666a322fbde66da86261c44
frischlander <frischlander@gmail.com>
2025-11-15 17:01:05 -0300

    11

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from caaordserv import views
+import views
 
 # Importações adicionais para gerenciamento programático
 from django.http import HttpResponse
@@ -12,10 +12,12 @@ import os # Importe o módulo os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('two_factor.urls')),
     path('authentication/', include('authentication.urls')),
     path('relatorios/', include('relatorios.urls')),
     path('', include('caaordserv.urls')),
     path('sobre/', views.sobre, name='sobre'),
+    path('two_factor/', include('two_factor.urls')),
+
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
