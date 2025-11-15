c939d7908dc0631935d62227915e32f4f238f92e
frischlander <frischlander@gmail.com>
2025-11-15 17:54:16 -0300

    15

--- urls.py
+++ urls.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('two_factor.urls')),
+    
+    # ESTA É A LINHA CORRETA PARA O 2FA
+    path('account/', include(('two_factor.urls', 'two_factor'))),
+    
+    # Suas outras rotas
     path('authentication/', include('authentication.urls')),
     path('relatorios/', include('relatorios.urls')),
     path('', include('caaordserv.urls')),
+    path('sobre/', views.sobre, name='sobre'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
