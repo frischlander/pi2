93bf805fd12f9866eef92710e67b9ebaf61f2206
frischlander <frischlander@gmail.com>
2025-11-15 13:34:46 -0300

    CORRECAO 4

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import UserTwoFactorAuth
 
-@admin.register(UserTwoFactorAuth)
-class UserTwoFactorAuthAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_enabled', 'created_at', 'updated_at')
-    list_filter = ('is_enabled', 'created_at')
-    search_fields = ('user__username', 'user__email')
-    readonly_fields = ('created_at', 'updated_at', 'secret_key')
+# Register your models here.
