136e7b820531d3fe7894a11482abe5eb18650aed
frischlander <frischlander@gmail.com>
2025-11-15 17:33:52 -0300

    14

--- settings.py
+++ settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django_otp.plugins.otp_static',
     'django_otp.plugins.otp_totp',
     'two_factor',
+    'two_factor.plugins.phonenumber',
     'caaordserv.apps.CaaordservConfig',
     'authentication.apps.AuthenticationConfig',
     'relatorios.apps.RelatoriosConfig',
